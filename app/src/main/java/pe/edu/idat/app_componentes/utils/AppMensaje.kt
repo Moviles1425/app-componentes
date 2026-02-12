2c43dcdc9226212086e73749ca150c0c5dea1861
Moviles1425 <moviles319@gmail.com>
2026-02-11 19:51:16 -0500

    avance del proyecto v1

---
+++ app/src/main/java/pe/edu/idat/app_componentes/utils/AppMensaje.kt
@@ -0,0 +1,22 @@
+package pe.edu.idat.app_componentes.utils
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import pe.edu.idat.app_componentes.R
+
+object AppMensaje {
+
+    fun mensajeSnackBar(vista: View, mensaje: String,
+                        tipoMensaje: TipoMensaje){
+        val snackbar = Snackbar.make(vista, mensaje, Snackbar.LENGTH_LONG)
+        if(tipoMensaje == TipoMensaje.ERROR){
+            snackbar.setBackgroundTint(
+                ContextCompat.getColor(MiApp.instance, R.color.error))
+        }else {
+            snackbar.setBackgroundTint(
+                ContextCompat.getColor(MiApp.instance, R.color.success))
+        }
+        snackbar.show()
+    }
+}
